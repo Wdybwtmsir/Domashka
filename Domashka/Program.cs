80896144e38c4afa65fe1c6c561d1c8895cec85b
khrom <khrom@DESKTOP-DA2UCIR>
2024-10-22 22:49:42 +0200

    Добавьте файлы проекта.

---
+++ Domashka/Program.cs
@@ -0,0 +1,14 @@
+﻿try
+{
+    Console.WriteLine("ВНИМАНИЕ!УЗРИТЕ!ТАБЛИЦА ПЕРЕВОДА ДЮЙМОВ В САНТИМЕТРЫ!!!!!!");
+    const double t=2.54;
+    for (int i = 1; i <= 20; i++) 
+    {
+        double c = i * t;
+        Console.WriteLine($"{i}\t{c:F2}");
+    }    
+}
+catch
+{
+    Console.WriteLine("Введите правильные значения!");
+}
